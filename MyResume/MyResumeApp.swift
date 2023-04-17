9dfd2a627f7a263386beecafe2857ad2807733ce
meng-che <henry19880720@gmail.com>
2023-04-18 07:48:31 +0800

    Initial Commit

---
+++ MyResume/MyResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyResumeApp.swift
+//  MyResume
+//
+//  Created by CHE on 2023/4/18.
+//
+
+import SwiftUI
+
+@main
+struct MyResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
